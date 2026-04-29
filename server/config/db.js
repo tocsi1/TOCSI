b6421af2ce9158823fc2342d669fe87c5b716a16
tocsi1 <tocsi.in@gmail.com>
2026-04-29 23:44:35 +0530

    fix DB duplicate issue

--- server/config/db.js
+++ server/config/db.js
@@ -1,28 +1,13 @@
-
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://tocsiadmin:Tocsi123@ac-cennser-shard-00-00.ebhubov.mongodb.net:27017,ac-cennser-shard-00-01.ebhubov.mongodb.net:27017,ac-cennser-shard-00-02.ebhubov.mongodb.net:27017/myWebsiteDB?tls=true&replicaSet=atlas-o9k1mm-shard-0&authSource=admin&retryWrites=true&w=majority");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
 };
-
-
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/tocsiDB");
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.error("MongoDB connection error:", error.message);
-    process.exit(1);
-  }
-};
-
 
 module.exports = connectDB;
