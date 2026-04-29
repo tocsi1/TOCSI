885958d4a24ef0e1141a7f4e4a2c38885c2ff32c
tocsi1 <tocsi.in@gmail.com>
2026-04-29 23:27:14 +0530

    fix server merge conflict

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require("dotenv").config();
 
 const express = require("express");
@@ -17,28 +16,8 @@ app.use(express.json());
 app.use("/contact", contactRoutes);
 app.use("/auth", authRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-=======
-require("dotenv").config();
+const PORT = process.env.PORT || 5000;
 
-const express = require("express");
-const cors = require("cors");
-const connectDB = require("./config/db");
-const contactRoutes = require("./routes/contactRoutes");
-const authRoutes = require("./routes/authRoutes");
-
-const app = express();
-
-connectDB();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/contact", contactRoutes);
-app.use("/auth", authRoutes);
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
->>>>>>> 338b2c97f7e46148fc166ca8077e8278819aa3ad
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
