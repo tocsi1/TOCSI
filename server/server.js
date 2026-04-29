b014012a8c0ea60b2b45d120337c0b0311c37744
tocsi1 <tocsi.in@gmail.com>
2026-04-30 00:09:59 +0530

    add backend home route

--- server/server.js
+++ server/server.js
@@ -18,6 +18,10 @@ app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("TOCSI backend is running successfully");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
