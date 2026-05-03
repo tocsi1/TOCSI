f3048de4c3452fc693450527e9b9c2ac7351bd74
tocsi1 <tocsi.in@gmail.com>
2026-05-03 14:53:00 +0530

    time chane

--- client/src/data/liveClassData.js
+++ client/src/data/liveClassData.js
@@ -4,7 +4,7 @@ const liveClasses = [
       id: "paper1",
       title: "UGC-NET Paper 1",
       mentor: "Arun Singh Katoch",
-      time: "Starts at 7:00 PM",
+      time: "Starts at 6:00 PM",
       icon: "⏰",
       videoId: "https://meet.google.com/qzm-ziuz-eqy",
      
